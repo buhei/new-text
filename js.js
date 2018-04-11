ea7263458aa41a914cf9ae1893645fa88e895460
HSQ <352982017@qq.com>
2018-04-11 10:22:54 +0800

    add js

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-js.js
+window.onload = function (){
+	var btn01 = document.getElementById("btn01");
+	var div01 = document.getElementById("div01");
+
+	btn01.onclick = function (){
+	   div01.style.backgroundColor = "yellow";
+	};
+};
